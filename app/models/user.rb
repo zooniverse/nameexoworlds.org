96dcd971aaf75738c947e4fad7baf63408ca70f9
Stuart <stuart@zooniverse.org>
2014-06-11 16:37:48 -0500

    oath user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,23 @@
 class User < ActiveRecord::Base
   has_many :votes
 
-  def self.from_omniauth(auth)
+  def self.find_or_create_from_auth_hash(auth)
     where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
   end
 
   def self.create_from_omniauth(auth)
+
+    binding.pry
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.name = auth["info"]["nickname"]
-      user.picture = auth["info"]["image"]
+      user.name = auth["info"]["name"]
+      user.avatar = auth["info"]["image"]
       user.oauth_token = auth["credentials"]["token"]
       user.oauth_expires_at = Time.at(auth["credentials"]["expires_at"]) if auth["provider"] == "facebook"
-      user.extra = auth
+      user.extra_info = auth
     end
   end
-
-  def populate_from_fb(auth)
-  end
-
-  def populate_from_twitter(auth)
-  end
-
 
   def inc_vote_count
     increment :vote_count
