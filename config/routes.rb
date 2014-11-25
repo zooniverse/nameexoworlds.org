a2409cafacf0cf80aab381a3d05f708c64b4ac25
Stuart <stuart@zooniverse.org>
2014-11-25 11:10:29 -0600

    home route

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Astronamer::Application.routes.draw do
 
   devise_for :clubs
   get '/auth/:provider/callback', to: 'sessions#create'
-  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
