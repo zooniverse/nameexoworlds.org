863cfa0fae76b9874b72596ecdf37c39afabf6af
Stuart <stuart@zooniverse.org>
2014-11-25 12:48:22 -0600

    session debug route (remove this soon)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,10 @@ class HomeController < ApplicationController
   def index
 
   end
+
+  def debug_session
+    respond_to do |format|
+      format.json {render :json => session.to_json}
+    end
+  end
 end
