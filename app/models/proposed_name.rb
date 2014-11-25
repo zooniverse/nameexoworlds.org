45e5945c82de8e6ff1015c2f2fd5d1114dde84c2
Stuart <stuart@zooniverse.org>
2014-11-25 11:21:23 -0600

    bug fix

--- app/models/proposed_name.rb
+++ app/models/proposed_name.rb
@@ -4,12 +4,12 @@ class ProposedName < ActiveRecord::Base
     has_many   :votes
 
     def inc_vote_count
-      increment :vote_count
+      increment! :vote_count
       planet.inc_vote_count, self
     end
 
     def dec_vote_count
-      decrement
+      decrement! :vote_count
       planet.dec_vote_count, self
     end
 end
