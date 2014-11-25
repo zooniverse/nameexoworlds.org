05109c9f60ca83ddcab7473e95ebb6333bb065aa
Stuart <stuart@zooniverse.org>
2014-11-25 11:25:00 -0600

    Weird heroku bug

--- app/models/proposed_name.rb
+++ app/models/proposed_name.rb
@@ -3,13 +3,13 @@ class ProposedName < ActiveRecord::Base
     belongs_to :planet
     has_many   :votes
 
-    def inc_vote_count
-      increment! :vote_count
-      planet.inc_vote_count, self
-    end
-
-    def dec_vote_count
-      decrement! :vote_count
-      planet.dec_vote_count, self
-    end
+    # def inc_vote_count
+    #   increment! :vote_count
+    #   planet.inc_vote_count, self
+    # end
+    #
+    # def dec_vote_count
+    #   decrement! :vote_count
+    #   planet.dec_vote_count, self
+    # end
 end
