07709372abe9074fb697a598bdaccf3f1ea0e684
Stuart <stuart@zooniverse.org>
2014-12-16 16:01:04 -0600

    systems controller

---
+++ app/controllers/systems_controller.rb
@@ -0,0 +1,29 @@
+class SystemsController < ApplicationController
+  # before_action :authenticate_club!
+
+  protect_from_forgery :except => [:add_club_vote, :remove_club_vote]
+
+  def add_club_vote
+    if @system = System.find(params[:id]) and (current_club || Club.first)
+      @system.vote_from_club(current_club || Club.first)
+      render :nothing => true
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
+  end
+
+  def remove_club_vote
+    if @system = System.find(params[:id]) and (current_club || Club.first)
+      @system.remove_vote_from_club(current_club || Club.first)
+      render :nothing => true
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
+  end
+
+  def show
+    @club = current_club || Club.first
+    @system = System.find(params[:id])
+  end
+
+end
