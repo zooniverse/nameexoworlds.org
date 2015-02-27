bb5d7559051e29f8e65daabfd53ffccf254af33b
Stuart <stuart@zooniverse.org>
2015-02-27 15:39:07 -0600

    skip migration for now

--- db/migrate/20150114212519_remove_email_uniq_on_club.rb
+++ db/migrate/20150114212519_remove_email_uniq_on_club.rb
@@ -1,8 +1,8 @@
-class RemoveEmailUniqOnClub < ActiveRecord::Migration
-  def up
-    sql = 'DROP INDEX index_clubs_on_email'
-    sql << ' ON clubs' if Rails.env == 'production' # Heroku pg
-    ActiveRecord::Base.connection.execute(sql)
-  end
-
-end
+# class RemoveEmailUniqOnClub < ActiveRecord::Migration
+#   def up
+#     sql = 'DROP INDEX index_clubs_on_email'
+#     sql << ' ON clubs' if Rails.env == 'production' # Heroku pg
+#     ActiveRecord::Base.connection.execute(sql)
+#   end
+#
+# end
