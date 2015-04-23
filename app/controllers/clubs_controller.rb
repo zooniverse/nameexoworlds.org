11706d52009c6c020782a577e05bba7310fe5324
Stuart <stuart@zooniverse.org>
2015-04-23 20:52:43 +0100

    removing debug

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,14 +1,15 @@
 class ClubsController < ApplicationController
-  before_action :authenticate_club!, except: [:show]
+  # before_action :authenticate_club!, except: [:show]
 
   def current
     respond_to do |format|
-      format.json {render :json => current_club.to_json}
+      format.json {render :json => (current_club || Club.first).to_json}
     end
   end
 
   def show
     @club = Club.find(params[:id])
+    @club = Club.first
     # if @club !=current_club
       # redirect_to '/'
     # end
