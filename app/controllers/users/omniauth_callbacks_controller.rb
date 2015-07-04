c88180e8fc5a7820b88f4f519165be3dd68006e4
Marten Veldthuis <marten@veldthuis.com>
2015-07-04 16:21:36 +0200

    add twitter/facebook login endpoints

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,28 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_filter :verify_authenticity_token
 
+  def twitter
+    @user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "Developer") if is_navigational_format?
+    else
+      set_flash_message(:notice, :failure, :kind => "Developer") if is_navigational_format?
+    end
+  end
+
+  def facebook
+    @user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "Developer") if is_navigational_format?
+    else
+      set_flash_message(:notice, :failure, :kind => "Developer") if is_navigational_format?
+    end
+  end
+
   def developer
     @user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
 
@@ -10,6 +32,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     else
       set_flash_message(:notice, :failure, :kind => "Developer") if is_navigational_format?
     end
-
   end
 end
