b6d3b144c32d73d1d887acbd897d2dd21740716a
Marten Veldthuis <marten@veldthuis.com>
2015-07-04 23:11:14 +0200

    Add google

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :twitter,  ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :facebook, ENV["FACEBOOK_APP_ID"] , ENV["FACEBOOK_APP_SECRET"]
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 end
